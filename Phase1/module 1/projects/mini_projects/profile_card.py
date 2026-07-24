c7c73d5f6d1d93990c069a651a6c48f4861646fc
Tapas-labs <tapusoni27@gmail.com>
2026-07-24 15:17:28 +0530

    edit

---
+++ Phase1/module 1/projects/mini_projects/profile_card.py
@@ -0,0 +1,9 @@
+print("="*40)
+print("      PERSONAL PROFILE")
+print("="*40)
+
+print("Name: Tapas")
+print("Age: 19")
+print("Occupation: Software Engineer")
+print("="*40)
+print("Thank you for visiting my profile!")
